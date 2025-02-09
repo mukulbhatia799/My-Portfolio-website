9c08c48887516628684d9ea1d275b361cfaae299
Mukul Bhatia <74963350+mukulbhatia799@users.noreply.github.com>
2025-02-09 11:15:31 +0530

    updated resume

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./home.css"
 // import { useTheme } from "../../context/ThemeContext";
 import Typewriter from 'typewriter-effect';
-import mukulResume from '../../images/MUKULBHATIA_12322024.pdf';
+import mukulResume from '../../images/0_MukulBhatia-26thDec-Resume.pdf';
 import { FiDownload } from 'react-icons/fi';
 import { FaGithub } from "react-icons/fa";
 // import { PiSunLight } from "react-icons/pi";
